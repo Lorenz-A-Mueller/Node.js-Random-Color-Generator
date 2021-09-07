9116ff1c81f37ff995c235c0a5301fa5ef753b1c
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-07 16:55:33 +0200

    Refactor and update readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 .) The program creates a random HEX color which is displayed to the console. The HEX code of the color is also depicted.
 
 .) It accepts an input of "red", "green", or "blue" and a second input of "light" or "dark". In this case, the resulting color will have a corresponding hue or brightness factor.
+
+.) Users can also enter "ask" as the first argument. This triggers two questions (enter color and brightness).
--- index.js
+++ index.js
@@ -1,55 +1,20 @@
   const chalk = require("chalk")
   const convert = require("color-convert")
-  const readline = require("readline");
+  const readline = require("node:readline");
 
+  let input1 = process.argv[2]
+  let input2 = process.argv[3]
 
+  // set completely random HSL-color to begin with (will stay that way if user doesn't enter data)
 
-   // input
-
-   let input1 = process.argv[2]
-   let input2 = process.argv[3]
-
-
-  // set completely random color
-
-   let hue = Math.floor(Math.random()*361)
-   const saturation = Math.floor(Math.random()*101)
-   let brightness = Math.floor(Math.random()*101)
-  // adaptColor to possible input
+  let hue = Math.floor(Math.random()*361)
+  const saturation = Math.floor(Math.random()*101)
+  let brightness = Math.floor(Math.random()*101)
 
 
 
 
-   function adaptColorAndPrint() {
-
-
-      // hue input (set hue to a fixed value)
-      if(input1 === "red") {
-      hue = 360
-    } else if (input1 === "green") {
-      hue = 120
-    } else if (input1 === "blue")  {
-      hue = 240
-    } else if (input1) {
-      console.log('first input must be "red", "green", or "blue"')
-    }
-
-  // brightness input (set brightness to a fixed value)
-
-  if (input2) {
-
-    if(input2 === "dark") {
-      brightness = 25
-    } else if (input2 === "light") {
-      brightness = 75
-    } else {
-      console.log('second entry must be "light" or "dark"')
-    }
- }
-
-    // convert to HEX
-const randomHex = convert.hsl.hex(hue, saturation, brightness)
-
+  function print(randomHex) {
 
     console.log(chalk.hex(randomHex)(`
       ###############################
@@ -61,23 +26,54 @@ const randomHex = convert.hsl.hex(hue, saturation, brightness)
       ###############################
       ###############################`))
 
+  }
 
-}
+   // adaptColor to possible input
+
+   function adaptColor() {
+
+    if(input1 === "red") {
+      hue = 360
+    } else if (input1 === "green") {
+      hue = 120
+    } else if (input1 === "blue")  {
+      hue = 240
+    } else if (input1) {
+      console.log('First input should be "red", "green", or "blue"  -> Ignored!')
+    }
 
 
-if(input1 === "ask") {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question(`Enter a color (red, green, or blue)\n`, askedColor => {
-    rl.question('Dark or light?\n', askedBrightness => {
-      input1 = askedColor
-      input2 = askedBrightness
-      rl.close()
-      adaptColorAndPrint()
+    if(input2 === "dark") {
+      brightness = 25
+    } else if (input2 === "light") {
+      brightness = 75
+    } else if (input2) {
+      console.log('Second input should be "light" or "dark"  --> Ignored!')
+    }
+
+    const randomHex = convert.hsl.hex(hue, saturation, brightness)   // convert to HEX and trigger printing
+    print(randomHex)
+
+   }
+
+
+
+
+// check for the ask-input
+
+  if(input1 === "ask") {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question(`Enter a color (red, green, or blue)\n`, askedColor => {  // adapt input1 and input2 according to input, then trigger adaptColor()
+      rl.question('Dark or light?\n', askedBrightness => {
+        input1 = askedColor
+        input2 = askedBrightness
+        rl.close()
+        adaptColor()
+       })
     })
-  })
-} else {
-  adaptColorAndPrint()
-}
+  } else {
+  adaptColor()
+  }
