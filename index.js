fe0e29f3d686e390d599cf6618f9d18e739fc8c7
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-07 13:47:22 +0200

    Add Yarn and ESlint; adapt code to changes suggested by ESlint

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-let randomHex;
 
-  let digitArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+  const digitArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
   function getRandomIndex() {
-    let randomIndex = Math.floor(Math.random() * 17);
+    const randomIndex = Math.floor(Math.random() * 17);
     return randomIndex;
   }
 
-    let randomHexArr = [];
+    const randomHexArr = [];
     for (let i = 0; i < 6; i++) {
       randomHexArr.push(digitArray[getRandomIndex()]);
     }
-    randomHex = '#' + randomHexArr.join('');
+    const randomHex = '#' + randomHexArr.join('');
     console.log(randomHex)
