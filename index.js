e931c123974dd8bde90a79c106573834599d66e6
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-07 15:14:31 +0200

    Adjust to the possibility of user input; create random number with hue; color-convert dependecy

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
   const chalk = require("chalk")
+  const convert = require("color-convert")
+
+   // input
+
+   const input1 = process.argv[2]
+   const input2 = process.argv[3]
 
 
-  const digitArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+  //set completely random color
 
-  function getRandomIndex() {
-    const randomIndex = Math.floor(Math.random() * 17);
-    return randomIndex;
-  }
+   let hue = Math.floor(Math.random()*361)
+   let saturation = Math.floor(Math.random()*101)
+   let brightness = Math.floor(Math.random()*101)
 
-    const randomHexArr = [];
-    for (let i = 0; i < 6; i++) {
-      randomHexArr.push(digitArray[getRandomIndex()]);
+    // hue input
+
+    if (input1) {
+
+      if(input1 === "red") {
+        hue = 360
+      } else if (input1 === "green") {
+        hue = 120
+      } else if (input1 === "blue")  {
+        hue = 240
+      } else {
+        console.log('first input must be \"red\", \"green\", or \"blue\"')
+      }
     }
-    const randomHex = '#' + randomHexArr.join('');
+    //brightness input
+
+    if (input2) {
+
+      if(input2 === "dark") {
+        brightness = 25
+      } else if (input2 === "light") {
+        brightness = 75
+      } else {
+        console.log('second entry must be \"light\" or \"dark\"')
+      }
+    }
+
+    const randomHSL = `(${hue}, ${saturation}% ${brightness}%)`
+    console.log(randomHSL)
+
+    // convert to HEX
+
+    let randomHex = convert.hsl.hex(hue, saturation, brightness)
+    console.log(randomHex)
+
+
+    // no input
+
+
+
     console.log(chalk.hex(randomHex)(`
       ###############################
       ###############################
       ###                         ###
-      ###         ${randomHex}         ###
+      ###         #${randomHex}         ###
       ###                         ###
       ###############################
       ###############################
