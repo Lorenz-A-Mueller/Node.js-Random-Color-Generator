e5d63a112e1fbaa5630e9e51a7a09200960d18dc
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-07 16:11:11 +0200

    Do first stretch goal

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
   const chalk = require("chalk")
   const convert = require("color-convert")
+  const readline = require("readline");
+
+
 
    // input
 
-   const input1 = process.argv[2]
+   let input1 = process.argv[2]
    const input2 = process.argv[3]
 
 
@@ -12,42 +15,40 @@
    let hue = Math.floor(Math.random()*361)
    const saturation = Math.floor(Math.random()*101)
    let brightness = Math.floor(Math.random()*101)
+  // adaptColor to possible input
 
-    // hue input (set hue to a fixed value)
 
-    if (input1) {
 
+
+   function adaptColorAndPrint() {
+
+
+      // hue input (set hue to a fixed value)
       if(input1 === "red") {
-        hue = 360
-      } else if (input1 === "green") {
-        hue = 120
-      } else if (input1 === "blue")  {
-        hue = 240
-      } else {
-        console.log('first input must be \"red\", \"green\", or \"blue\"')
-      }
-    }
-    // brightness input (set brightness to a fixed value)
-
-    if (input2) {
-
-      if(input2 === "dark") {
-        brightness = 25
-      } else if (input2 === "light") {
-        brightness = 75
-      } else {
-        console.log('second entry must be \"light\" or \"dark\"')
-      }
+      hue = 360
+    } else if (input1 === "green") {
+      hue = 120
+    } else if (input1 === "blue")  {
+      hue = 240
+    } else if (input1) {
+      console.log('first input must be "red", "green", or "blue"')
     }
 
+  // brightness input (set brightness to a fixed value)
+
+  if (input2) {
+
+    if(input2 === "dark") {
+      brightness = 25
+    } else if (input2 === "light") {
+      brightness = 75
+    } else {
+      console.log('second entry must be "light" or "dark"')
+    }
+ }
 
     // convert to HEX
-
-    const randomHex = convert.hsl.hex(hue, saturation, brightness)
-
-
-    // no input
-
+const randomHex = convert.hsl.hex(hue, saturation, brightness)
 
 
     console.log(chalk.hex(randomHex)(`
@@ -59,3 +60,23 @@
       ###############################
       ###############################
       ###############################`))
+
+
+}
+
+
+if(input1 === "ask") {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question(`Enter a color (red, green, or blue)\n`, askedColor => {
+    input1 = askedColor
+    console.log(input1)
+    rl.close()
+    adaptColorAndPrint()
+
+  })
+} else {
+  adaptColorAndPrint()
+}
