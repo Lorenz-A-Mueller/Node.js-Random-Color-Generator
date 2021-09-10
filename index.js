b990fc96515632d8b9c07d6c30eeae6165217a25
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-10 09:01:31 +0200

    Improve program according to Jose`s/Ines's suggestions (correct naming of varialbe/input); no hard-coding of printing feature; program asks for new color if wrong one does not exist; add more colors

--- index.js
+++ index.js
@@ -5,58 +5,115 @@ const readline = require('readline-sync');
 let input1 = process.argv[2];
 let input2 = process.argv[3];
 
-// set completely random HSL-color to begin with (will stay that way if user doesn't enter data)
+// get hue, saturation and lightness in order to construct a random HSL-color (will stay that way if user doesn't enter data)
+// restrict saturation and lightness to values between 30 and 70
 
 let hue = Math.floor(Math.random() * 361);
-const saturation = Math.floor(Math.random() * 101);
-let brightness = Math.floor(Math.random() * 101);
+let saturation = 30 + Math.floor(Math.random() * 41);
+let lightness = 30 + Math.floor(Math.random() * 41);
 
-function print(randomHex) {
-  console.log(
-    chalk.hex(randomHex)(`
-      ###############################
-      ###############################
-      ###                         ###
-      ###         #${randomHex}         ###
-      ###                         ###
-      ###############################
-      ###############################
-      ###############################`),
-  );
+// function askHue. Will be called if the user inputs "ask". Ask for a color and resets hue values according to the input.
+
+function askHue() {
+  let repeatQuestion = false;
+  input1 = readline.question(`Please enter a color\n`).toLowerCase();
+
+  switch (input1) {
+    case 'red':
+      hue = 0;
+      break;
+    case 'orange':
+      hue = 30;
+      break;
+    case 'yellow':
+      hue = 60;
+      break;
+    case 'olive':
+      hue = 90;
+      break;
+    case 'green':
+      hue = 120;
+      break;
+    case 'teal':
+      hue = 150;
+      break;
+    case 'cyan':
+      hue = 180;
+      break;
+    case 'blue':
+      hue = 210;
+      break;
+    case 'violet':
+      hue = 240;
+      break;
+    case 'purple':
+      hue = 270;
+      break;
+    case 'magenta':
+      hue = 300;
+      break;
+    case 'scarlet':
+      hue = 330;
+      break;
+
+    // extra cases:
+
+    case 'white':
+      lightness = 100;
+      break;
+    case 'black':
+      lightness = 0;
+      break;
+    case 'gray':
+      saturation = 0;
+      break;
+
+    // if input is not a defined color, call the function recursively (until user input is valid)
+
+    default:
+      console.log('Color not found. Please try another.');
+      repeatQuestion = true;
+  }
+  if (repeatQuestion) askHue();
 }
 
-// adaptColor to possible input
+// check if "ask" was put in by the user (if so, call askHue)
 
-function adaptColor() {
-  if (input1 === 'red') {
-    hue = 360;
-  } else if (input1 === 'green') {
-    hue = 120;
-  } else if (input1 === 'blue') {
-    hue = 240;
-  } else if (input1) {
-    console.log('First input should be "red", "green", or "blue"  -> Ignored!');
-  }
+if (input1 === 'ask') askHue();
+
+// if there was a first input and the color was neither white nor black
+// ask for light/dark  [light -> 70; dark -> 30 lightness]
+
+if (input1 && lightness !== 100 && lightness !== 0) {
+  input2 = readline.question(`Light or dark?\n`).toLowerCase();
 
   if (input2 === 'dark') {
-    brightness = 25;
+    lightness = 30;
   } else if (input2 === 'light') {
-    brightness = 75;
+    lightness = 70;
   } else if (input2) {
     console.log('Second input should be "light" or "dark"  --> Ignored!');
   }
-
-  const randomHex = convert.hsl.hex(hue, saturation, brightness); // convert to HEX and trigger printing
-  print(randomHex);
 }
 
-// check for the ask-input
+// convert to HEX (with the color-convert module)
 
-if (input1 === 'ask') {
-  input1 = readline.question(`Enter a color (red, green, or blue)\n`);
-  input2 = readline.question(`Bright or dark?\n`);
+const randomColor = convert.hsl.hex(hue, saturation, lightness);
 
-  adaptColor();
-} else {
-  adaptColor();
-}
+// print the color
+
+const fullRow = '#'.repeat(31) + '\n';
+const holeRow = '#'.repeat(3) + ' '.repeat(25) + '#'.repeat(3) + '\n';
+const centerRow =
+  '#'.repeat(3) +
+  ' '.repeat(10) +
+  randomColor +
+  ' '.repeat(9) +
+  '#'.repeat(3) +
+  '\n';
+
+console.log(
+  chalk.hex(randomColor)(
+    `${fullRow.repeat(3)}${holeRow}${centerRow}${holeRow}${fullRow.repeat(3)}`,
+  ),
+);
